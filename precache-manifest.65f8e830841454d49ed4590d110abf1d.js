f501be801f938144512b1c77fc851dac6924a7a4
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-07-05 09:53:35 -0400

    Updates

---
+++ precache-manifest.65f8e830841454d49ed4590d110abf1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a734c36f5199934a2020e1ea9aa1a8c5",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "0d55e8eeeabaf35bd195",
+    "url": "/crwn-clothing/static/css/main.d40f95ab.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/crwn-clothing/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0d55e8eeeabaf35bd195",
+    "url": "/crwn-clothing/static/js/main.f994338f.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  }
+]);
