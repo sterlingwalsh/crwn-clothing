a577db63361e6394460be97b6f54599b1d8f0973
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-07-15 14:36:09 -0400

    finished implimenting shop collection page, added link to pages on respective preview titles

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <div>
         <Header />
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,12 +6,15 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './collection.styles.scss';
 
 const CollectionPage = ({ match, collection }) => {
+  const { title, items } = collection;
   return (
     <div className='collection-page'>
-      <h2>{collection.title}</h2>
-      {collection.items.map(item => (
-        <CollectionItem key={item.id} item={item} />
-      ))}
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
